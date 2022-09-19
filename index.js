a56dc87c83d20eccd776766415b8b3582d59acbe
Justin McCraw <jmlmccraw@gmail.com>
2022-09-19 15:56:17 -0400

    functions imported

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const app = require('./app')
 const PORT = process.env.PORT || 3000
+const {getJobs, getCityInfo} = require('./util')
+
+// app.get('/public', (req, res) => {
+//   res.json()
+// })
 
 app.listen(PORT, () => {
   console.log('App started:')
