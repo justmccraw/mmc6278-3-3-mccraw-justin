3b2ef7715b6dadb1b980a71706bba07488bd82b7
Justin McCraw <jmlmccraw@gmail.com>
2022-09-22 10:47:01 -0400

    statically serve public folder

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const app = require('./app')
-const PORT = process.env.PORT || 3000
 const {getJobs, getCityInfo} = require('./util')
+const path = require('path')
+const express = require('express')
+const PORT = process.env.PORT || 3000
 
+// TODO: import the getCityInfo and getJobs functions from util.js (Done!)
+// TODO: Statically serve the public folder
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + './public/index.html'))
+})
+// TODO: declare the GET route /api/city/:city
 // app.get('/public', (req, res) => {
 //   res.json()
 // })
+// This endpoint should call getCityInfo and getJobs and return
+// the result as JSON.
+// The returned JSON object should have two keys:
+// cityInfo (with value of the getCityInfo function)
+// jobs (with value of the getJobs function)
+// If no city info or jobs are found,
+// the endpoint should return a 404 status
+
+
+
 
 app.listen(PORT, () => {
   console.log('App started:')
